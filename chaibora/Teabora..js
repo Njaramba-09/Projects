785f9b3d37873abcd80fd73f54f12cc426045cf8
brendanjaramba <njarambabrenda5@gmail.com>
2025-06-06 13:47:12 +0300

    Project

---
+++ chaibora/Teabora..js
@@ -0,0 +1,35 @@
+// Declare constants 
+const w = 200;
+const m = 50;
+const t = 1;
+const s = 2;
+
+// Declaring variables
+let a1;
+let b1;
+let c1;
+let d1;
+let input;
+let u;
+
+// Declare function
+function  calculateChaiboraIngridents(u) {
+  a = m * u;
+  b= t * u;
+  c = s * u;
+  d = w * u;
+  return { a, b, c, d };
+}
+
+// Input
+input = prompt("Enter the number of cups of tea you want to make:");
+u = parseInt(input);
+
+// Output
+ calculateChaiboraIngrident(u);
+console.log(`To make ${u} cups of tea, you need:`);
+console.log(`${a} ml of water`);
+console.log(`${b} ml of milk`);
+console.log(`${c} tsp of tea`);
+console.log(`${d} tsp of sugar`);
+console.log("Enjoy your tea!");
